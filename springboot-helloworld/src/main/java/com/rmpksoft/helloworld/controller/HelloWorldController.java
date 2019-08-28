7dde8883068406acf48ceff3c15474b75867cba3
pradeepkrathnayaka <pradeepkrathnayaka@gmail.com>
2019-08-28 15:12:16 +0800

    updates with spring boot 2.1.7 and junit5 test libraries

--- springboot-helloworld/src/main/java/com/rmpksoft/helloworld/controller/HelloWorldController.java
+++ springboot-helloworld/src/main/java/com/rmpksoft/helloworld/controller/HelloWorldController.java
@@ -1,17 +1,24 @@
 package com.rmpksoft.helloworld.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rmpksoft.helloworld.service.HelloWorldService;
+
 @RestController
+@RequestMapping("greetings")
 public class HelloWorldController {
 
 	@Value("${message}")
 	private String message;
+	
+	private HelloWorldService helloWorldService;
 
-	@RequestMapping("/")
-	public String helloWorld() {
+	@RequestMapping("/{msg}")
+	public String helloWorld( @PathVariable("msg") String msg) {
+		System.out.println("Path Variable message : " + msg);
 		return message;
 	}
 
---
+++ springboot-helloworld/src/main/java/com/rmpksoft/helloworld/service/HelloWorldService.java
@@ -0,0 +1,8 @@
+package com.rmpksoft.helloworld.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldService {
+
+}
